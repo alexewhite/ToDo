4dfba83784b7302ee62540161580c2e684884b6d
Alex <alex.eliz.white@gmail.com>
2025-08-08 12:52:52 -0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
